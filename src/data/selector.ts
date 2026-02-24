471beaf337871eb8e99b2103e728498518aa210e
Aidan Timson <aidan@timmo.dev>
2026-02-24 16:37:08 +0000

    Date formatter C

--- src/data/selector.ts
+++ src/data/selector.ts
@@ -74,6 +74,7 @@ export type Selector =
   | TTSSelector
   | TTSVoiceSelector
   | UiActionSelector
+  | UiClockDateFormatSelector
   | UiColorSelector
   | UiStateContentSelector
   | BackupLocationSelector;
@@ -503,6 +504,10 @@ export interface UiActionSelector {
     actions?: UiAction[];
     default_action?: UiAction;
   } | null;
+}
+
+export interface UiClockDateFormatSelector {
+  ui_clock_date_format: {} | null;
 }
 
 export interface UiColorSelector {
